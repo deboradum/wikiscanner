300832d73e4809ed3079f0df4da07c70ffb3fbf8
Pepijn <pvwpvw@protonmail.com>
2022-11-02 20:57:17 +0100

    small changes

--- scrapeIP.py
+++ scrapeIP.py
@@ -6,11 +6,21 @@ import re
 # data visualiseren met hoeveel procent van welke landen etc.
 # % of revisions annonymous
 # Only include revisions made by IPs in API query
+# 249 calls b2b error/ crash afhandelen
+# parse monthly wikipedia datadumps
 
 IPV4REGEX = '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 IPV6REGEX = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
 URL = "https://en.wikipedia.org/w/api.php"
-PARAMS = {
+
+
+
+class PageRevs:
+    def __init__(self):
+        self.anon_revs = []
+        self.total_revs = 0
+        self.ses = requests.Session()
+        self.params = {
         "action": "query",
         "prop": "revisions",
         "titles": "2020 Nagorno-Karabakh war",
@@ -18,20 +28,14 @@ PARAMS = {
         "format": "json"
     }
 
-
-class PageRevs:
-    def __init__(self):
-        self.revs = []
-        self.total = 0
-        self.ses = requests.Session()
-
     # Gets all revisions made by
     def get_revs(self, rvcont:str=""):
+        # Adds continue value to params if needed.
         if rvcont != "":
-            new_params = PARAMS.copy()
+            new_params = self.params.copy()
             new_params["rvcontinue"] = rvcont
         else:
-            new_params = PARAMS.copy()
+            new_params = self.params.copy()
 
         # Gets json encoded content of response of the get request.
         data = self.ses.get(url=URL, params=new_params).json()
@@ -42,14 +46,15 @@ class PageRevs:
         # gets all revisions made by an annonymous user.
         revisions = data["query"]["pages"][page_id]["revisions"]
         for rev in revisions:
-            self.total += 1
+            self.total_revs += 1
             matchipv4 = re.search(IPV4REGEX, rev["user"])
             matchipv6 = re.search(IPV6REGEX, rev["user"])
             if matchipv4 or matchipv6:
-                if rev not in self.revs:
-                    self.revs.append(rev)
+                if rev not in self.anon_revs:
+                    self.anon_revs.append(rev)
 
-        # Gets continue value
+        # Gets continue value and calls function again if not all data is read.
+        # Maximum API calls is 249.
         rvcontnew = data["continue"]["rvcontinue"]
         if rvcontnew != "":
             try:
@@ -61,8 +66,10 @@ class PageRevs:
 def main():
     p = PageRevs()
     p.get_revs()
-    print(p.total)
-    print(len(p.revs))
+
+    print(p.total_revs)
+    print(len(p.anon_revs))
+    print(p.anon_revs[1])
 
 
 if __name__ == "__main__":
