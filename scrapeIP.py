1f80b1f2e49aa93737cd86b10bd1ef4c14e0244a
Pepijn <pvwpvw@protonmail.com>
2022-11-02 19:26:38 +0100

    getting all annonymous revisions working

--- scrapeIP.py
+++ scrapeIP.py
@@ -2,11 +2,10 @@ import requests
 import sys
 import re
 
-
 # TODO
-# Make checking work for all Wikipedia languages
 # data visualiseren met hoeveel procent van welke landen etc.
-# IPv4 en IPv6 regex
+# % of revisions annonymous
+# Only include revisions made by IPs in API query
 
 IPV4REGEX = '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 IPV6REGEX = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
@@ -18,28 +17,45 @@ PARAMS = {
         "rvlimit": 500,
         "format": "json"
     }
+TOTAL = 0
 
-def main():
+# Gets all revisions made by
+def get_revs(revs:list, rvcont:str=""):
     ses = requests.Session()
-    revs = []
+    if rvcont != "":
+        new_params = PARAMS.copy()
+        new_params["rvcontinue"] = rvcont
+    else:
+        new_params = PARAMS.copy()
 
     # Gets json encoded content of response of the get request.
-    data = ses.get(url=URL, params=PARAMS).json()
+    data = ses.get(url=URL, params=new_params).json()
 
-    # NOG IETS ANDERS OP VINDEN OM PAGE ID TE KRIJGEN
-    x = data["query"]["pages"]
-    for y in x:
-        page_id = y
-        break
+    # Gets page ID
+    page_id = list(data["query"]["pages"].keys())[0]
 
-    revisions = data["query"]["pages"][page_id]["revisions"]
     # gets all revisions made by an annonymous user.
+    revisions = data["query"]["pages"][page_id]["revisions"]
     for rev in revisions:
         matchipv4 = re.search(IPV4REGEX, rev["user"])
         matchipv6 = re.search(IPV6REGEX, rev["user"])
         if matchipv4 or matchipv6:
-            revs.append(rev)
-    print(revs)
+            if rev not in revs:
+                revs.append(rev)
+
+     # Gets continue value
+    rvcontnew = data["continue"]["rvcontinue"]
+    if rvcontnew != "":
+        try:
+            get_revs(revs, rvcont=rvcontnew)
+        except Exception:
+            pass
+
+
+def main():
+    revs = []
+
+    get_revs(revs)
 
 
 if __name__ == "__main__":
