fc7d6a931d0fdb7f0ff9ad9a75f86ba5ab925ba2
Pepijn <pvwpvw@protonmail.com>
2022-11-02 21:25:01 +0100

    scraping more props

--- scrapeIP.py
+++ scrapeIP.py
@@ -6,13 +6,15 @@ import re
 # data visualiseren met hoeveel procent van welke landen etc.
 # % of revisions annonymous
 # Only include revisions made by IPs in API query
-# 249 calls b2b error/ crash afhandelen
+# >249 calls b2b error/ crash afhandelen
 # parse monthly wikipedia datadumps
+# IPINFO database
+# EDIT database
+# TAGSPEREDIT database
 
 IPV4REGEX = '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 IPV6REGEX = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
 URL = "https://en.wikipedia.org/w/api.php"
-
 
 
 class PageRevs:
@@ -25,6 +27,7 @@ class PageRevs:
         "prop": "revisions",
         "titles": "2020 Nagorno-Karabakh war",
         "rvlimit": 500,
+        "rvprop": "ids|flags|timestamp|user|userid|size|comment|tags",
         "format": "json"
     }
 
@@ -67,9 +70,14 @@ def main():
     p = PageRevs()
     p.get_revs()
 
-    print(p.total_revs)
-    print(len(p.anon_revs))
+    # print(p.total_revs)
+    # print(len(p.anon_revs))
+    print(p.anon_revs[0])
     print(p.anon_revs[1])
+    print(p.anon_revs[2])
+    print(p.anon_revs[3])
+    print(p.anon_revs[4])
+    print(p.anon_revs[5])
 
 
 if __name__ == "__main__":
