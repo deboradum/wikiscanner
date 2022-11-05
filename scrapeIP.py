c029f2ace41bfa57389da823f4b5058487876902
Pepijn <pvwpvw@protonmail.com>
2022-11-05 15:01:39 +0100

    added IP api functionality

--- scrapeIP.py
+++ scrapeIP.py
@@ -19,7 +19,6 @@ import re
 IPV4REGEX = '^(?:[0-9]{1,3}\.){3}[0-9]{1,3}$'
 IPV6REGEX = '(([0-9a-fA-F]{1,4}:){7,7}[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,7}:|([0-9a-fA-F]{1,4}:){1,6}:[0-9a-fA-F]{1,4}|([0-9a-fA-F]{1,4}:){1,5}(:[0-9a-fA-F]{1,4}){1,2}|([0-9a-fA-F]{1,4}:){1,4}(:[0-9a-fA-F]{1,4}){1,3}|([0-9a-fA-F]{1,4}:){1,3}(:[0-9a-fA-F]{1,4}){1,4}|([0-9a-fA-F]{1,4}:){1,2}(:[0-9a-fA-F]{1,4}){1,5}|[0-9a-fA-F]{1,4}:((:[0-9a-fA-F]{1,4}){1,6})|:((:[0-9a-fA-F]{1,4}){1,7}|:)|fe80:(:[0-9a-fA-F]{0,4}){0,4}%[0-9a-zA-Z]{1,}|::(ffff(:0{1,4}){0,1}:){0,1}((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])|([0-9a-fA-F]{1,4}:){1,4}:((25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9])\.){3,3}(25[0-5]|(2[0-4]|1{0,1}[0-9]){0,1}[0-9]))'
 WIKI_API_URL = "https://en.wikipedia.org/w/api.php"
-IP_API_URL = "http://ip-api.com/batch?fields=status,message,continent,continentCode,country,countryCode,region,regionName,city,district,zip,lat,lon,isp,org,as,proxy,query"
 
 class PageRevs:
     def __init__(self):
@@ -95,8 +94,9 @@ class PageRevs:
 def get_ip_info(ips):
     s = requests.Session()
 
-    data = s.get(url=IP_API_URL, data=json.dumps(ips))
-    print(data)
+    for ip in ips:
+        data = s.get(url=f"https://ipapi.co/{ip}/json/").json()
+        print(data)
 
     return
 
