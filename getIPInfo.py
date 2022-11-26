c1128ddcfc2004cebddfc29f505124540c5e842d
Pepijn <pvwpvw@protonmail.com>
2022-11-26 19:55:44 +0100

    fixed ipapi ratellimit bug

--- getIPInfo.py
+++ getIPInfo.py
@@ -1,4 +1,5 @@
 import requests
+import time
 
 # Returns a dictionary containg information about the IP address.
 def get_ips_info(ips):
@@ -6,7 +7,11 @@ def get_ips_info(ips):
     ips_info = []
 
     for ip in ips:
+        time.sleep(0.24)
         data = s.get(url=f"https://ipapi.co/{ip}/json/").json()
-        ips_info.append(data)
+        if data.get("reason") != 'RateLimited':
+            ips_info.append(data)
+        else:
+            print(f"Rate limited by ipapi. IP {ip} not added.")
 
     return ips_info
